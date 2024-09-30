9a33a6f3634cde63461e4dbdff168b48514f5495
Alone <675061370@qq.com>
2024-09-30 11:38:36 +0800

    修复搜索特殊符号报错

--- app/index/route/index.php
+++ app/index/route/index.php
@@ -4,7 +4,7 @@
 use think\facade\Route;
 
 
-Route::get('s/<name>-<page?>-<cate?>','index/index/list',[],['name' => '.+', 'id' => '\d+', 'cate' => '\d+']);
+Route::get('s/<name>-<page?>-<cate?>', 'index/index/list')->pattern(['name' => '[^-]+', 'id' => '\d+', 'cate' => '\d+']);
 Route::get('d/:id','index/index/detail');
 Route::get('sitemap.xml', 'index/sitemap/index');
 
